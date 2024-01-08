d15be5bc0d7c94d973416afeb2d9cc58ed45d1a5
Gerbrand Bosch <gerbrandb@gmail.com>
2024-01-08 15:17:25 +0100

    changed getcwd to laravel's base_path

--- config/sql-export.php
+++ config/sql-export.php
@@ -6,10 +6,10 @@ return [
     /**
      * This is the path to the Laravel migrations directory.
      */
-    'laravel_migrations_path' => env('SQL_EXPORT_LARAVEL_MIGRATIONS_PATH', getcwd() . '/database/migrations'),
+    'laravel_migrations_path' => env('SQL_EXPORT_LARAVEL_MIGRATIONS_PATH', base_path() . '/database/migrations'),
 
     /**
      * The export tool exports the Laravel migrations to SQL migration in this directory.
      */
-    'sql_migrations_path' => env('SQL_EXPORT_SQL_MIGRATIONS_PATH', getcwd() . '/database/sql'),
+    'sql_migrations_path' => env('SQL_EXPORT_SQL_MIGRATIONS_PATH', base_path() . '/database/sql'),
 ];
