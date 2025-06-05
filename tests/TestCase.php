ea7183f43024adbcf95158653a8fa3858875bccf
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-06-05 15:48:27 +0200

    Define TestCase class to be used by tests

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinVWS\SqlExporter\Tests;
+
+use MinVWS\SqlExporter\SqlExporterServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            SqlExporterServiceProvider::class,
+        ];
+    }
+
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', '55322'),
+            'database' => env('DB_DATABASE', 'db_test'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'password'),
+        ]);
+    }
+}
